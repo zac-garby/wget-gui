75316c37d008cf116633260f18e0f1fae67195e1
Zac Garby <zacgarby@icloud.com>
2017-06-23 15:43:48 +0100

    Initial Commit

---
+++ wget-gui/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  wget-gui
+//
+//  Created by Zac G on 23/06/2017.
+//  Copyright © 2017 Zac G. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ wget-gui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wget-gui
+//
+//  Created by Zac G on 23/06/2017.
+//  Copyright © 2017 Zac G. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
